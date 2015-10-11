2918b84ab2ab5cb5b5ed19815084951d9c3cab58
Samuel Gélineau <gelisam+github@gmail.com>
2015-10-11 18:35:55 -0400

    rename to parseLexemeW
    to match the upcoming parseLexemeV

--- src/Main.hs
+++ src/Main.hs
@@ -21,7 +21,7 @@ data Whitespace
 type LexemeW = Either Whitespace Lexeme
 
 -- |
--- >>> mapM_ print $ lexer "for (let i of [1,2,3]) {...}"
+-- >>> mapM_ print $ parseLexemeW "for (let i of [1,2,3]) {...}"
 -- Right (Alphanum "for")
 -- Left (Blank " ")
 -- Right (Open "(")
@@ -43,25 +43,25 @@ type LexemeW = Either Whitespace Lexeme
 -- Right (Open "{")
 -- Right (Symbol "...")
 -- Right (Close "}")
-lexer :: String -> [LexemeW]
-lexer [] = []
-lexer (c:cs) | isAlphaNum c = case lexer cs of
+parseLexemeW :: String -> [LexemeW]
+parseLexemeW [] = []
+parseLexemeW (c:cs) | isAlphaNum c = case parseLexemeW cs of
     Right (Alphanum s) : ls -> Right (Alphanum (c:s)) : ls
     ls                      -> Right (Alphanum [c]  ) : ls
-lexer ('\n':cs) = Left Newline : lexer cs
-lexer (c:cs) | isSpace c = case lexer cs of
+parseLexemeW ('\n':cs) = Left Newline : parseLexemeW cs
+parseLexemeW (c:cs) | isSpace c = case parseLexemeW cs of
     Left (Blank s) : ls -> Left (Blank (c:s)) : ls
     ls                  -> Left (Blank [c]  ) : ls
-lexer ('/':'/':cs) = Left (Comment comment) : lexer cs'
+parseLexemeW ('/':'/':cs) = Left (Comment comment) : parseLexemeW cs'
   where
     (comment, cs') = break (/= '\n') cs
-lexer ('{':cs) = Right (Open  "{") : lexer cs
-lexer ('[':cs) = Right (Open  "[") : lexer cs
-lexer ('(':cs) = Right (Open  "(") : lexer cs
-lexer (')':cs) = Right (Close ")") : lexer cs
-lexer (']':cs) = Right (Close "]") : lexer cs
-lexer ('}':cs) = Right (Close "}") : lexer cs
-lexer (c:cs) = case lexer cs of
+parseLexemeW ('{':cs) = Right (Open  "{") : parseLexemeW cs
+parseLexemeW ('[':cs) = Right (Open  "[") : parseLexemeW cs
+parseLexemeW ('(':cs) = Right (Open  "(") : parseLexemeW cs
+parseLexemeW (')':cs) = Right (Close ")") : parseLexemeW cs
+parseLexemeW (']':cs) = Right (Close "]") : parseLexemeW cs
+parseLexemeW ('}':cs) = Right (Close "}") : parseLexemeW cs
+parseLexemeW (c:cs) = case parseLexemeW cs of
     Right (Symbol s) : ls -> Right (Symbol (c:s)) : ls
     ls                    -> Right (Symbol [c]  ) : ls
 
